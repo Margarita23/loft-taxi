505411bc861d75d6e9353ff8a74bf741fdc1f5e4
Marharita Andriukhina <margarita.andriukhina@gmail.com>
2020-04-06 19:00:52 +0300

    start

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
-import React, {Component} from 'react';
-// import logo from './logo.svg';
-import './App.scss';
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-import Login from './pages/login/login';
-import Header from './components/header/header';
-import Profile from './pages/profile/profile';
-
-
-class App extends Component {
-   render() {
-      return (
-         <>
-            <Login/>
-            <Header/>
-            <Profile/>
-         </>
-      );
-   }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,18 +4,14 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-//
-import { theme } from "loft-taxi-mui-theme"; // Импортируем саму тему
-import { MuiThemeProvider } from "@material-ui/core/styles";
-//
-
 ReactDOM.render(
   <React.StrictMode>
-    <MuiThemeProvider theme={theme}>
-      <App />
-    </MuiThemeProvider>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
 
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
